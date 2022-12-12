c056673225568c51db49ba3cc0f8bb1c799f22da
Juliano Candido <julianosgs@yahoo.com.br>
2022-12-12 16:54:26 -0300

    Criando as entidades do modelo de dominio

---
+++ TesteAPI_Iara/TesteAPI_Iara/Models/Cotacao.cs
@@ -0,0 +1,18 @@
+﻿namespace TesteAPI_Iara.Models
+{
+    public class Cotacao
+    {
+        public int CotacaoId { get; set; }
+        public string? CnpjComprador { get; set; }
+        public string? CnpjFornecedor { get; set; }
+        public int? NumeroCotacao  { get; set; }
+        public DateTime DataCotacao { get; set; }
+        public DateTime DataEntregaCotacao { get; set; }
+        public string? Cep { get; set; }
+        public string? Logradouro { get; set; }
+        public string? Complemento { get; set; }
+        public string? Bairro { get; set; }
+        public char Uf { get; set; }
+        public string? Observacao { get; set; }
+    }
+}
---
+++ TesteAPI_Iara/TesteAPI_Iara/Models/CotacaoItem.cs
@@ -0,0 +1,13 @@
+﻿namespace TesteAPI_Iara.Models
+{
+    public class CotacaoItem
+    {
+        public int CotacaoItemId { get; set; }
+        public string? Descricao { get; set; }
+        public int NumeroItem { get; set; }
+        public decimal Preco { get; set; }
+        public float Quantidade { get; set; }
+        public string? Marca { get; set; }
+        public string? Unidade { get; set; }
+    }
+}
